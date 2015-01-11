0d732db2e2ed6612605d12f955719c231947a182
NewPagodi <m0072@thaleswell.com>
2015-01-11 07:15:49 -0600

    checking for file existance

--- plot1.R
+++ plot1.R
@@ -1,3 +1,17 @@
+#if the needed file isn't in the working directory
+#unzip or dl as needed.
+if (!file.exists("household_power_consumption.txt")){
+  if (!file.exists("exdata_data_household_power_consumption.zip")){
+    message("Downloading data")
+    download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",destfile="exdata_data_household_power_consumption.zip")
+    unzip("exdata_data_household_power_consumption.zip")
+  }
+  else{
+    unzip("exdata_data_household_power_consumption.zip")
+  }
+}
+
+
 library(sqldf)
 f <- file("household_power_consumption.txt")
 bigdf <- sqldf("select * from f", dbname = tempfile(), file.format = list(sep=";",header = T, row.names = F))
